3caf75a54a03dc34f532cd6e90bd11d11c75c096
Dharani-Sri <103064835+Dharani-Sri@users.noreply.github.com>
2022-04-08 19:29:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ def main():
     if mode == "Dataset":
         st.subheader("Let us explore the dataset")
         st.write("It is necessary to work on collected data, pre-process them in order to obtain a consistent dataset and then extract the most relevant features. Here we can see the raw dataset....")
-        if st.button("Load the dataset"):
+        if st.button("Load the dataset",disabled=False):
             df = load_data()
             st.dataframe(df)
             time.sleep(10)
