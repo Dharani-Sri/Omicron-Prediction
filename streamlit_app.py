7c05745476d3d9c5150d0bcbc81213d162cf4142
Dharani-Sri <103064835+Dharani-Sri@users.noreply.github.com>
2022-04-08 19:53:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,9 +83,9 @@ def main():
     if mode == "Dataset":
         st.subheader("Let us explore the dataset")
         st.write("It is necessary to work on collected data, pre-process them in order to obtain a consistent dataset and then extract the most relevant features. Here we can see the raw dataset....")
-    if st.sidebar.button("Load the Dataset"):
-        df = pd.read_csv(url)
-        st.dataframe(df)
+        if st.sidebar.button("Load the Dataset"):
+            df = pd.read_csv(url)
+            st.dataframe(df)
             
     if mode == "Model Prediction":
         st.subheader("Prediction chart over week")
