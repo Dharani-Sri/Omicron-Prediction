132712427e7f48786462537e3b7a264d974adf27
Dharani-Sri <103064835+Dharani-Sri@users.noreply.github.com>
2022-04-08 17:25:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def processing():
     return df
     
 def cmap1():
-    statewise = pd.read_csv('/Users/USER/statewise_cases.csv')
+    statewise = pd.read_csv('statewise_cases.csv')
     fig = px.choropleth_mapbox(statewise,
     geojson="https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson",
     featureidkey='properties.ST_NM',
@@ -43,7 +43,7 @@ def cmap1():
     return fig
 
 def cmap2():
-    statewise = pd.read_csv('/Users/USER/statewise_cases.csv')
+    statewise = pd.read_csv('statewise_cases.csv')
     fig = px.choropleth_mapbox(statewise,
     geojson="https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson",
     featureidkey='properties.ST_NM',
@@ -96,7 +96,7 @@ def main():
         st.image(img)
         st.subheader("Model Prediction Table")
         model_predictions = prediction()       
-        model_predictions = pd.read_csv('/Users/USER/file1.csv')
+        model_predictions = pd.read_csv('img/file1.csv')
         st.dataframe(model_predictions)
         
     if mode == "Timeseries Forecasting":
