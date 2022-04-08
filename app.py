a72ef71bc75ec623f7809c01d15d24e99c6a9f2c
Dharani-Sri <103064835+Dharani-Sri@users.noreply.github.com>
2022-04-08 17:54:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.header("Exploratory Data Analysis ,Prediction and Timeseries forecasting of O
 st.write("-------")
 
 sidebar_options =  ["Introduction","Dataset","Visualization","Model Prediction","Timeseries Forecasting"]    
-radio_options = ["Daily Cases","Correlation","Timestamp","Chloropleth Maps"]
+radio_options = ["Daily Cases","Correlation","Timestamp","Chloropleth"]
     
 def load_data():
     df=pd.read_csv(url)
@@ -136,9 +136,10 @@ def main():
             st.pyplot(hm.figure)
             
         if choice == "Timestamp":
+            st.subheader("Daily progress of New Cases")
             st.image("img/progress.PNG")
             
-        if choice == "Chloropleth Maps":
+        if choice == "Chloropleth":
             fig1 = cmap1()
             st.plotly_chart(fig1)
             fig2 = cmap2()
