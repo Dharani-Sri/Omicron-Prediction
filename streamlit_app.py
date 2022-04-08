344abaee8472a745bb6e0f0637c256485591fe1d
Dharani-Sri <103064835+Dharani-Sri@users.noreply.github.com>
2022-04-08 18:43:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from PIL import Image
 
-url="https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
+url="https://ourworldindata.org/explorers/coronavirus-data-explorer?zoomToSelection=true&time=2020-03-01..latest&facet=none&pickerSort=desc&pickerMetric=new_cases_smoothed_per_million&Metric=Confirmed+cases&Interval=7-day+rolling+average&Relative+to+Population=true&Color+by+test+positivity=false&country=IND~USA~GBR~CAN~DEU~FRA"
 
 st.header("Exploratory Data Analysis ,Prediction and Timeseries forecasting of Omicron in India")
 st.write("-------")
@@ -82,8 +82,9 @@ def main():
         
     if mode == "Dataset":
         st.subheader("Let us explore the dataset")
-        df = load_data()
-        st.dataframe(df)  
+        if st.button("Load th dataset"):
+            df = load_data()
+            st.dataframe(df)  
         st.write("It is necessary to work on collected data, pre-process them in order to obtain a consistent dataset and then extract the most relevant features.")
             
     if mode == "Model Prediction":
